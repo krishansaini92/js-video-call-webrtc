b504bd95e7a5e72acc2a79633a1e5d047cd24bb8
Krishan Saini <krishan@thinkscoopinc.com>
2022-07-26 19:54:39 +0900

    [#1.7~1.8 nickname] sending nickname through socket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,28 @@
 // alert("hi");
 
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+// const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nickname");
 
 // const socket = new WebSocket("ws://localhost:3000");
 const socket = new WebSocket(`ws://${window.location.host}`);   // socket (frontend) : 서버로의 연결
+
+// 소켓으로 메시지를 보내기 전, 메시지의 종류를 알려주기 위해 메시지를 json object로 묶어서 보내줌 (단, 보낼 때는 string 형식으로 보내야한다.)
+function makeStrMessageFromJson(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    // console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -26,7 +37,22 @@ function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
     // console.log(input.value);
-    socket.send(input.value);
+    // socket.send(input.value);
+    socket.send(makeStrMessageFromJson("new_message", input.value));    // 여러 종류의 메시지를 구분하기 위해 전송하는 메시지 타입을 지정 (Json 형식)
+
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+
+    input.value = "";
+}
+
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeStrMessageFromJson("nickname", input.value));
+    input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -30,12 +30,25 @@ const sockets = [];         // 몇 명이 연결되었는지 체크하기 위해
 // wss.on("connection", handleConnection);
 wss.on("connection", (socket) => {              // to keep listening for events
     sockets.push(socket);   // 연결이 이루어질 때마다 추가 (여러 브라우저에서 접속할 경우)
+    socket["nickname"] = "Anonymous";
+    // console.log(socket);
+
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from Browser"));
     socket.on("message", (message) => {
         // console.log(message.toString('utf8'));
         // socket.send(message.toString('utf8'));
-        sockets.forEach((eachSocket) => eachSocket.send(message.toString('utf8')));
+        // sockets.forEach((eachSocket) => eachSocket.send(message.toString('utf8')));
+
+        const parsedMessage = JSON.parse(message.toString('utf8'));      // string 형식으로 온 메시지를 json object로 파싱 (보낼 때 json object 형식으로 묶어줬기 때문에 가능)
+        switch (parsedMessage.type) {
+            case "new_message":
+                // sockets.forEach((eachSocket) => eachSocket.send(parsedMessage.payload));
+                sockets.forEach((eachSocket) => eachSocket.send(`${socket.nickname}: ${parsedMessage.payload}`));
+            case "nickname":
+                // console.log(parsedMessage.payload);
+                socket["nickname"] = parsedMessage.payload;
+        }
     });
     // socket.send("hello!!!");
 });
